600208427f3f7d962267277822f57f54d7b7a25a
Dmytro Mazurev <dmytro.mazurev@mera.com>
2025-03-07 14:05:48 +0200

    Input update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,42 +12,18 @@ public class Main {
     public static <T> void main(String[] args) throws IOException {
 
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        Comparator<Integer> intComparator = Integer::compare;
         BinaryTreeImpl<Integer> tree = new BinaryTreeImpl<Integer>();
-
-
 
 
         while (true) {
             System.out.println("Choose the operation");
-            System.out.println("1: Insert Node\n2: Delete Node\n3: Draw Tree\n4: Read from file\n5: Stop Program");
+            System.out.println("1: Read from file\n2: Read from terminal\n3: Draw Tree\n4: Stop Program");
             String operation = r.readLine();
             switch (operation) {
                 case "1" -> {
-                    System.out.println("Write a data to put in");
-                    String data = r.readLine();
-                    try {
-                        Integer intData = Integer.parseInt(data);
-                        tree.insertNode(intData);
-                    } catch (NumberFormatException e) {
-                        System.out.println("Incorrect format");
-                    }
-                }
-                case "2" -> {
-                    System.out.println("Write a data to delete");
-                    String data = r.readLine();
-                    try {
-                        Integer intData = Integer.parseInt(data);
-                        tree.deleteNode(intData);
-                    } catch (NumberFormatException e) {
-                        System.out.println("Incorrect format");
-                    }
-                }
-                case "3" -> tree.draw();
-                case "4" -> {
-                    System.out.println("Write file name");
+                    System.out.println("Write full file name");
                     String filename = r.readLine();
-                    File file = new File("src/main/resources/" + filename);
+                    File file = new File(filename);
                     try {
                         Scanner myReader = new Scanner(file);
                         while (myReader.hasNextLine()) {
@@ -69,15 +45,50 @@ public class Main {
                                 }
                             }
                         }
-                    } catch (FileNotFoundException e){
+                    } catch (FileNotFoundException e) {
                         System.out.println("Can't find file with this name");
                     }
                 }
-                case "5"-> {return;}
+                case "2" -> {
+                    while (true) {
+                        System.out.println("1: Insert Node\n2: Delete Node\n3: Draw Tree\n4: Stop Program");
+                        String operation1 = r.readLine();
+                        switch (operation1) {
+                            case "1" -> {
+                                System.out.println("Write a data to put in");
+                                String data = r.readLine();
+                                try {
+                                    Integer intData = Integer.parseInt(data);
+                                    tree.insertNode(intData);
+                                } catch (NumberFormatException e) {
+                                    System.out.println("Incorrect format");
+                                }
+                            }
+                            case "2" -> {
+                                System.out.println("Write a data to delete");
+                                String data = r.readLine();
+                                try {
+                                    Integer intData = Integer.parseInt(data);
+                                    tree.deleteNode(intData);
+                                } catch (NumberFormatException e) {
+                                    System.out.println("Incorrect format");
+                                }
+                            }
+                            case "3" -> tree.draw();
+                            case "4" -> {
+                                return;
+                            }
+                        }
+                    }
+                }
+                case "3" -> tree.draw();
+                case "4" -> {
+                    return;
+                }
+
             }
         }
 
 
-        
     }
 }
